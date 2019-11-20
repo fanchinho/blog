b402593271ce0a1464171c858f5dbe8636506ac1
fanchinho <57795423+fanchinho@users.noreply.github.com>
2019-11-20 11:06:47 +0100

    champs obligatoires création articles

--- View/backend/createPost.php
+++ View/backend/createPost.php
@@ -9,15 +9,16 @@
                 <div class="comments-form">
                     <div class="comment-respond">
                         <form  action="index.php?action=addPost" method="post" class="comment-form" enctype="multipart/form-data">
-                            <label for="fileUpload">Fichier:</label>
+                            <label for="fileUpload">Image de l'article <small style="color:red;"> *</small></label>
                             <input type="file" name="photo" id="fileUpload"><br><br>
-                            <label for="title">Renseignez le titre de l'article</label>
+                            <label for="title">Renseignez le titre de l'article <small style="color:red;"> *</small></label>
                             <input type="text" id="title" name="title">
                             <label for="tags">Tags (séparez par une virgule)</label>
                             <input type="text" id="tags" name="tags">
-                            <label for="content">Contenu de l'article</label>
+                            <label for="content">Contenu de l'article <small style="color:red;"> *</small></label>
                             <textarea name="content" height="300px" class="post-content"> </textarea>
                             <input type="submit" value="Valider la création de l'article">
+                            <small style="color:red;"> * Champs obligatoires</small>
                         </form><!-- .comment-form -->
                     </div><!-- .comment-respond -->
                 </div><!-- .comments-form -->
